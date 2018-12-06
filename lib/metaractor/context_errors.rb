e1aaaccc82c1bc156b7518802266b33232d8b921
Ryan Schlesinger <ryan@ryanschlesinger.com>
2018-12-06 14:57:51 -0800

    Allow non-string structured errors

--- lib/metaractor/context_errors.rb
+++ lib/metaractor/context_errors.rb
@@ -8,26 +8,37 @@ module Metaractor
       super
     end
 
-    def fail_with_error!(message:)
-      add_error(message: message)
+    def fail_with_error!(message: nil, **args)
+      add_error(message: message, **args)
       fail!
     end
 
-    def fail_with_errors!(messages:)
-      add_errors(messages: messages)
+    def fail_with_errors!(messages: [], errors: [])
+      add_errors(messages: messages, errors: errors)
       fail!
     end
 
-    def add_error(message:)
-      add_errors(messages: Array(message))
+    def add_error(message: nil, **args)
+      if message.present?
+        add_errors(messages: Array(message))
+      else
+        add_errors(errors: [**args])
+      end
     end
 
-    def add_errors(messages:)
+    def add_errors(messages: [], errors: [])
       self.errors += messages
+      self.errors += errors
     end
 
     def error_messages
-      errors.join("\n")
+      errors.map do |error|
+        if error.respond_to?(:has_key?) && error.has_key?(:title)
+          error[:title].to_s
+        else
+          error.to_s
+        end
+      end.join("\n")
     end
   end
 end
